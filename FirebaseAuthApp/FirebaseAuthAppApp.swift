d418d5ed40dae934cd127fc2e828aaefce124567
Tuyenkaka2005 <nguyendangtuyen2005@gmail.com>
2026-05-13 22:32:39 -0700

    Initial Commit

---
+++ FirebaseAuthApp/FirebaseAuthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthAppApp.swift
+//  FirebaseAuthApp
+//
+//  Created by Tuyen on 13/5/26.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
